d6b9a125761e63637b2eeba3a1220e68a9f3938f
avinashp22 <avinashp22@gmail.com>
2023-12-11 09:59:38 +0000

    avinash-week14-nopcommerce-demo-sw-5

--- src/test/java/com/nopcommerce/demo/testsuite/ComputerPageTest.java
+++ src/test/java/com/nopcommerce/demo/testsuite/ComputerPageTest.java
@@ -4,7 +4,6 @@ import com.nopcommerce.demo.customlisteners.CustomListeners;
 import com.nopcommerce.demo.pages.HomePage;
 import com.nopcommerce.demo.pages.LoginPage;
 import com.nopcommerce.demo.testbase.BaseTest;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -24,34 +23,20 @@ public class ComputerPageTest extends BaseTest {
 
     @Test
     public void verifyUserShouldNavigateToComputerPageSuccessfully() {
-        homePage.clickOnLoginLink();
-        String expectedMessage = "Welcome, Please Sign In!";
-        String actualMessage = loginPage.getWelcomeText();
-        Assert.assertEquals(expectedMessage, actualMessage, "Login page not displayed");
+
+
     }
 
     @Test
     public void verifyUserShouldNavigateToDesktopsPageSuccessfully() {
-        homePage.clickOnLoginLink();
-        loginPage.enterEmailId("prime123@gmail.com");
-        loginPage.enterPassword("prime123");
-        loginPage.clickOnLoginButton();
-        String expectedErrorMessage = "Login was unsuccessful. Please correct the errors and try again.\n"
-                + "No customer account found";
-        String actualErrorMessage = loginPage.getErrorMessage();
-        Assert.assertEquals( expectedErrorMessage, actualErrorMessage,"Error message not displayed");
+
+
     }
 
     @Test
     public void verifyThatUserShouldBuildYouOwnComputerAndAddThemToCartSuccessfully(String processor, String ram, String hdd, String os, String software){
-        homePage.clickOnLoginLink();
-        loginPage.enterEmailId("prime123@gmail.com");
-        loginPage.enterPassword("prime123");
-        loginPage.clickOnLoginButton();
-        String expectedErrorMessage = "Login was unsuccessful. Please correct the errors and try again.\n"
-                + "No customer account found";
-        String actualErrorMessage = loginPage.getErrorMessage();
-        Assert.assertEquals( expectedErrorMessage, actualErrorMessage,"Error message not displayed");
+
+
     }
 
 }
--- src/test/java/com/nopcommerce/demo/testsuite/RegisterPageTest.java
+++ src/test/java/com/nopcommerce/demo/testsuite/RegisterPageTest.java
@@ -4,7 +4,6 @@ import com.nopcommerce.demo.customlisteners.CustomListeners;
 import com.nopcommerce.demo.pages.HomePage;
 import com.nopcommerce.demo.pages.LoginPage;
 import com.nopcommerce.demo.testbase.BaseTest;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -24,34 +23,20 @@ public class RegisterPageTest extends BaseTest {
 
     @Test
     public void verifyUserShouldNavigateToRegisterPageSuccessfully() {
-        homePage.clickOnLoginLink();
-        String expectedMessage = "Welcome, Please Sign In!";
-        String actualMessage = loginPage.getWelcomeText();
-        Assert.assertEquals(expectedMessage, actualMessage, "Login page not displayed");
+
+
     }
 
     @Test
     public void verifyThatFirstNameLastNameEmailPasswordAndConfirmPasswordFieldsAreMandatory() {
-        homePage.clickOnLoginLink();
-        loginPage.enterEmailId("prime123@gmail.com");
-        loginPage.enterPassword("prime123");
-        loginPage.clickOnLoginButton();
-        String expectedErrorMessage = "Login was unsuccessful. Please correct the errors and try again.\n"
-                + "No customer account found";
-        String actualErrorMessage = loginPage.getErrorMessage();
-        Assert.assertEquals( expectedErrorMessage, actualErrorMessage,"Error message not displayed");
+
+
     }
 
     @Test
     public void verifyThatUserShouldCreateAccountSuccessfully() {
-        homePage.clickOnLoginLink();
-        loginPage.enterEmailId("prime123@gmail.com");
-        loginPage.enterPassword("prime123");
-        loginPage.clickOnLoginButton();
-        String expectedErrorMessage = "Login was unsuccessful. Please correct the errors and try again.\n"
-                + "No customer account found";
-        String actualErrorMessage = loginPage.getErrorMessage();
-        Assert.assertEquals( expectedErrorMessage, actualErrorMessage,"Error message not displayed");
+
+
     }
 
 }
